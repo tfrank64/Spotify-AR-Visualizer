f59bee47a7ffc589cae5d47e22468dff5e5db022
Taylor Franklin <taylorleefranklin@gmail.com>
2018-01-26 23:57:32 -0600

    first commit

---
+++ MusicAR/OrbViewController.swift
@@ -0,0 +1,117 @@
+import UIKit
+import SceneKit
+import ARKit
+// place on center horizontal plane,make the size of ottoman.
+// add particle emission in different colors and intensities
+// clean up, fix force unwrapping
+class OrbViewController: UIViewController, ARSCNViewDelegate {
+    
+    var sceneView: ARSCNView!
+    var planes = [OverlayPlane]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sceneView = ARSCNView(frame: self.view.frame)
+        self.view.addSubview(self.sceneView)
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        
+        let scene = SCNScene()
+        let sphere = SCNSphere(radius: 0.2)
+        
+        let sphereNode = SCNNode()
+        sphereNode.geometry = sphere
+        sphereNode.position = SCNVector3(0,0.1,-1)
+        
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.lightingModel = SCNMaterial.LightingModel.physicallyBased
+        let materialFilePrefix = "greasy-pan-2"
+//        let materialFilePrefix = "rustediron-streaks"
+        sphereMaterial.diffuse.contents = UIImage(named: "\(materialFilePrefix)-albedo.png")
+        sphereMaterial.roughness.contents = UIImage(named: "\(materialFilePrefix)-roughness.png")
+        sphereMaterial.metalness.contents = UIImage(named: "\(materialFilePrefix)-metal.png")
+        sphereMaterial.normal.contents = UIImage(named: "\(materialFilePrefix)-normal.png")
+        sphereNode.geometry?.materials = [sphereMaterial]
+        sphereNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 10, y: 1, z: 1, duration: 30)))
+        
+        scene.rootNode.addChildNode(sphereNode)
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        insertLighting(position: SCNVector3(1.0,2.0,-1))
+    }
+    
+    private func insertLighting(position :SCNVector3) {
+        
+        let directionalLight = SCNLight()
+        directionalLight.type = .directional
+        
+        let directionalNode = SCNNode()
+        directionalNode.name = "DirectionalNode"
+        directionalNode.light = directionalLight
+        directionalNode.position = position
+        
+        directionalNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(-90), 0, GLKMathDegreesToRadians(-45))
+        self.sceneView.scene.rootNode.addChildNode(directionalNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let estimate: ARLightEstimate? = sceneView.session.currentFrame?.lightEstimate
+        if estimate == nil {
+            return
+        }
+        let directionalNode = self.sceneView.scene.rootNode.childNode(withName: "DirectionalNode", recursively: true)
+        directionalNode?.light?.intensity = (estimate?.ambientIntensity)!
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+        }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // TODO: add logic to  handle orb placement
+        configuration.planeDetection = .horizontal
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    
+}
+
