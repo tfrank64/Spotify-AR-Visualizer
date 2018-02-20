a4143a7cf3f01c23c1f9c430feeff17ccc06e513
Taylor Franklin <taylorleefranklin@gmail.com>
2018-02-19 19:18:53 -0600

    got spotify sdk working and loggin in

--- MusicAR/AppDelegate.swift
+++ MusicAR/AppDelegate.swift
@@ -12,35 +12,33 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var auth = SPTAuth()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        auth.redirectURL = URL(string: "music-ar-login://callback")
+        auth.sessionUserDefaultsKey = "currentSession"
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if auth.canHandle(auth.redirectURL) {
+            auth.handleAuthCallback(withTriggeredAuthURL: url, callback: { (error, session) in
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+                guard error == nil, let spotifySession = session else {
+                    print("error: \(String(describing: error?.localizedDescription))")
+                    return
+                }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+                let userDefaults = UserDefaults.standard
+                let sessionData = NSKeyedArchiver.archivedData(withRootObject: spotifySession)
+                userDefaults.set(sessionData, forKey: "SpotifySession")
+                userDefaults.synchronize()
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+                NotificationCenter.default.post(name: Notification.Name(rawValue: "loginSuccessfull"), object: nil)
+            })
+            return true
+        }
+        return false
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
-
--- MusicAR/MusicPickerViewController.swift
+++ MusicAR/MusicPickerViewController.swift
@@ -7,27 +7,119 @@
 //
 
 import UIKit
+import SafariServices
 
 class MusicPickerViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
     var auth = SPTAuth.defaultInstance()!
-    var session:SPTSession!
+    var session: SPTSession!
     var player: SPTAudioStreamingController?
     var loginUrl: URL?
+    var authViewController: UIViewController!
+    var latestPlaylistList: SPTPlaylistList?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         sdkSetup()
+        NotificationCenter.default.addObserver(self, selector: #selector(updateAfterFirstLogin), name: Notification.Name(rawValue: "loginSuccessfull"), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func sdkSetup() {
-        auth.clientID = ""
-        auth.redirectURL = "" // TODO: finish this
+        auth.clientID = "0064a012b4474a3e9255941f27f1f27d"
+        auth.redirectURL = URL(string: "music-ar-login://callback")
+        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPublicScope]
+        loginUrl = auth.spotifyWebAuthenticationURL()
+        
+        if let currentSession = getStoredSession() {
+            print("session Valid: \(currentSession.expirationDate)")
+            self.session = currentSession
+            initializePlayer(authSession: self.session)
+            setupPlaylists()
+        }
+    }
+    
+    func getStoredSession() -> SPTSession? {
+        if let sessionObj: Any = UserDefaults.standard.object(forKey: "SpotifySession") as Any?,
+            let sessionDataObj = sessionObj as? Data,
+            let mySession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as? SPTSession,
+            mySession.isValid() {
+            return mySession
+        }
+        return nil
+    }
+    
+    @objc func updateAfterFirstLogin() {
+        if let firstTimeSession = getStoredSession() {
+            self.authViewController.dismiss(animated: true, completion: nil)
+            self.session = firstTimeSession
+            initializePlayer(authSession: self.session)
+        }
     }
 
+    @IBAction func login(_ sender: Any) {
+        if let authURL = loginUrl {
+            self.authViewController = SFSafariViewController(url: authURL)
+            self.present(self.authViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func initializePlayer(authSession: SPTSession) {
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self
+            self.player!.delegate = self
+            try! player!.start(withClientId: auth.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
+        }
+    }
+    
+    func setupPlaylists() {
+        // if already setup, don't do again
+        if self.loginButton.isHidden {
+            return
+        }
+        self.loginButton.isHidden = true
+        
+        SPTPlaylistList.playlists(forUser: self.session.canonicalUsername, withAccessToken: self.session.accessToken)
+        { (error, playlistData) in
+            guard error == nil, let playlists = playlistData as? SPTPlaylistList, let partialPlaylists = playlists.items as? [SPTPartialPlaylist] else {
+                print("Error getting playlists: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            if self.latestPlaylistList == nil {
+                self.latestPlaylistList = playlists
+            }
+            
+            for playlist in partialPlaylists {
+                print("plist: \(playlist.name)")
+                // add to data list
+            }
+        }
+    }
+    
+    // create method to detect when scrolled to bottom
+    // if so, do call to get next playlist and add to data object (not created yet)
+    // continually getlatest playlist and call nextPlaylist...
+    // if no more pages, ignore call.
+}
+
+extension MusicPickerViewController: SPTAudioStreamingDelegate, SPTAudioStreamingPlaybackDelegate {
+    
+    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+        print("logged in")
+        setupPlaylists()
+//        self.player?.playSpotifyURI("spotify:track:2pJZ1v8HezrAoZ0Fhzby92", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+//            if (error != nil) {
+//                print("Error playing song: \(String(describing: error?.localizedDescription))")
+//            }
+//            print("playing!")
+//        })
+    }
+    
 }
