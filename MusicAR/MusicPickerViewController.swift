a478d3945aee15844b5ca4cf4a6574e22515e5e5
Taylor Franklin <taylorleefranklin@gmail.com>
2018-02-25 18:44:03 -0600

    WIP, creating tableview interface and populating with data

--- MusicAR/MusicPickerViewController.swift
+++ MusicAR/MusicPickerViewController.swift
@@ -11,16 +11,20 @@ import SafariServices
 
 class MusicPickerViewController: UIViewController {
     
+    @IBOutlet weak var playlistTableView: UITableView!
     @IBOutlet weak var loginButton: UIButton!
     var auth = SPTAuth.defaultInstance()!
     var session: SPTSession!
     var player: SPTAudioStreamingController?
     var loginUrl: URL?
     var authViewController: UIViewController!
-    var latestPlaylistList: SPTPlaylistList?
+//    var latestPlaylistList: SPTPlaylistList?
+    var masterPlaylistList = [SPTPartialPlaylist]()
+    var nextPlaylistPageUrl: URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        playlistTableView.estimatedRowHeight = 120
         sdkSetup()
         NotificationCenter.default.addObserver(self, selector: #selector(updateAfterFirstLogin), name: Notification.Name(rawValue: "loginSuccessfull"), object: nil)
     }
@@ -84,6 +88,7 @@ class MusicPickerViewController: UIViewController {
             return
         }
         self.loginButton.isHidden = true
+        self.playlistTableView.isHidden = false
         
         SPTPlaylistList.playlists(forUser: self.session.canonicalUsername, withAccessToken: self.session.accessToken)
         { (error, playlistData) in
@@ -91,15 +96,31 @@ class MusicPickerViewController: UIViewController {
                 print("Error getting playlists: \(String(describing: error?.localizedDescription))")
                 return
             }
-            
-            if self.latestPlaylistList == nil {
-                self.latestPlaylistList = playlists
-            }
-            
+            print("hasNextPage? \(playlists.hasNextPage) and nextPage: \(playlists.nextPageURL)")
+            self.masterPlaylistList = partialPlaylists
+            self.nextPlaylistPageUrl = playlists.hasNextPage ? playlists.nextPageURL : nil
             for playlist in partialPlaylists {
-                print("plist: \(playlist.name)")
-                // add to data list
+                print("playlist name: \(playlist.name)")
+                print("and uri: \(playlist.uri)")
             }
+            // TODO: get main queue and reload table view
+        }
+    }
+    
+    func addNextPlaylistPage() {
+        if let nextPageUrl = self.nextPlaylistPageUrl {
+            let urlRequest = URLRequest(url: nextPageUrl)
+            SPTRequest.sharedHandler().perform(urlRequest, callback: { (error, response, data) in
+                guard error == nil, let resp = response, let playlistData = data else {
+                    print("Error getting next playlist: \(String(describing: error?.localizedDescription))")
+                    return
+                }
+                print("theresponse: \(resp)")
+                let stringdata = String(data: playlistData, encoding: .utf8)
+                print("stringdata: \(String(describing: stringdata))")
+            })
+        } else {
+            print("Error: Next page URL is nil, there are no more playlists to load.")
         }
     }
     
@@ -120,6 +141,35 @@ extension MusicPickerViewController: SPTAudioStreamingDelegate, SPTAudioStreamin
 //            }
 //            print("playing!")
 //        })
+    }
+    
+}
+
+extension MusicPickerViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.masterPlaylistList.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Select Playlist"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "playlistCell", for: indexPath)
+        let playlistItem = self.masterPlaylistList[indexPath.item]
+        cell.textLabel?.text = playlistItem.name
+        cell.imageView?.contentMode = UIViewContentMode.scaleAspectFill
+        cell.imageView?.image = UIImage(named: "doge")
+        return cell
     }
     
 }
